0110d996f4f004e9acc3e230a9d658423e393784
bronzekennen <sotoskand3@gmail.com>
2023-07-31 17:34:53 +0300

    Created home screen

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,17 @@
+import "../styles/Home.css"
+
 export default function Home() {
+  function handleClick() {
+    console.log(<h1>GAMW TO ANTITHEO SOU EXW KARTA</h1>)
+  }
   return (
-    <h1>Ayo whassup gang</h1>
+    <>
+      <div className="home"/>
+      <div className="container">
+        <h1>All Greek Rocket League Tournaments in one place</h1>
+        <button id="get-started-button" onClick={handleClick}>Get started</button>
+        <button id="available-tournaments">Available tournaments</button>
+      </div>
+    </>
   )
 }
