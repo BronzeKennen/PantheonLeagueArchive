bf2e9aff61eb93bf13ca47478a6e85ee0fcff5e5
bronzekennen <sotoskand3@gmail.com>
2023-07-31 15:04:24 +0300

    added styling to navbar, more routes

--- src/pages/NavBar.jsx
+++ src/pages/NavBar.jsx
@@ -1,11 +1,16 @@
 import "../styles/NavBar.css"
 export default function NavBar() {
+
+  const path = window.location.pathname;
+
   return (
         <nav className="NavBar">
             <ul>
-                <li><a href="./Home" className="site-title ">RLTG</a></li>
-                <li><a href="./Tournaments">Tournaments</a></li>
-                <li><a href="./signin" className="sign-in">Sign in</a></li>
+                <li><a href="./Home" className={path === '/Home' ? 'active site-title' : 'site-title'}>RLTG</a></li>
+                <li><a href="./Tournaments" className={path === '/Tournaments' ? 'active rest' : 'rest'}>Tournaments</a></li>
+            </ul>
+            <ul>
+                <li><a href="./signin" className={path === '/signin' ? 'active sign-in' : 'sign-in'}>Sign in</a></li>
             </ul>
         </nav>
     )
