c2e941883895cc2520a23503abcf4d1537607b66
bronzekennen <sotoskand3@gmail.com>
2023-08-01 15:42:17 +0300

    Added homepage responsiveness

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,40 @@
 import "../styles/Home.css"
+import { motion } from 'framer-motion'
 
 export default function Home() {
-  function handleClick() {
-    console.log(<h1>GAMW TO ANTITHEO SOU EXW KARTA</h1>)
-  }
   return (
     <>
       <div className="home"/>
       <div className="container">
-        <h1>All Greek Rocket League Tournaments in one place</h1>
-        <button id="get-started-button" onClick={handleClick}>Get started</button>
-        <button id="available-tournaments">Available tournaments</button>
+        <motion.h1 className="header"
+        animate={{
+          opacity: 1,
+          y:260
+        }}
+        transition={{duration:1}}
+        >All Greek Rocket League Tournaments in one place</motion.h1>
+        <span>
+        <motion.button className="header-button" id="get-started-button"
+        whileHover={{
+          scale: 1.05,
+          boxShadow: '0px 0px 1px 1px orange'
+        }}
+        animate={{
+          opacity: 1
+        }}
+        transition={{opacity: {duration:1, delay:1}}}
+        >Get started</motion.button>
+        <motion.button className="header-button" id="available-tournaments"
+        whileHover={{
+          scale: 1.05,
+          boxShadow: '1px 1px 3px 3px purple'
+        }}
+        animate={{
+          opacity: 1
+        }}
+        transition={{opacity: {duration:1, delay:1}}}
+        >Available tournaments</motion.button>
+        </span>
       </div>
     </>
   )
