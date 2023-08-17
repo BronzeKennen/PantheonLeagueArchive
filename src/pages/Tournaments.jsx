9a322920cb6db9657c7045c83a0997d41eee988b
bronzekennen <sotoskand3@gmail.com>
2023-08-17 13:53:20 +0300

    added tournament component

--- src/pages/Tournaments.jsx
+++ src/pages/Tournaments.jsx
@@ -1,5 +1,23 @@
+import "../styles/Tournaments.css"
+
 export default function Tournaments() {
   return (
-    <div>Tournaments</div>
+    <>
+    <h1 id="tour-title">Tournaments</h1>
+    <div className="tournament-container">
+      <TourComponent/>
+    </div>
+    </>
+  )
+}
+
+function TourComponent() {
+  return(
+    <div className="tour-component">
+      <h2>Tournament Name</h2>
+      <span>Num of participants</span>
+      <span>Date</span>
+      <button>VIEW</button>
+    </div>
   )
 }
