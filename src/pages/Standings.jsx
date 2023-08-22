c77ec477c517862d784173f8ac269be9bfb71805
BronzeKennen <sotoskand3@gmail.com>
2023-08-22 14:54:47 +0300

    recreated standings

--- src/pages/Standings.jsx
+++ src/pages/Standings.jsx
@@ -1,5 +1,6 @@
 import "../styles/Standings.css"
 import { createClient } from '@supabase/supabase-js'
+import {motion} from 'framer-motion'
 const supabase = createClient('https://cwxskbsjnovkyqeloumw.supabase.co', import.meta.env.VITE_SUPABASE_API_KEY)
 
 let { data: teams, error } = await supabase
@@ -9,14 +10,39 @@ let { data: teams, error } = await supabase
 export default function Standings() {
     teams.sort((a,b) => b.wins - a.wins)
     return (
-        teams.map((team) => {
-            return(<TeamStanding key={team.name} team={team}/>)
-        })
+        <>
+        <h1 id="standings-header">Standings</h1>
+        {teams.map((team, index) => {
+            return(<TeamStanding index={index} key={team.name} team={team}/>)
+        })}
+        </>
     )
 }
 
-function TeamStanding(team) {
+function TeamStanding(props) {
+    const {team, index} = props;
     return(
-        <h2>{team.team.name}</h2>
+        <motion.div className="team-stand"
+            animate={{
+                opacity: 1
+            }}
+            transition={{opacity: {duration:1, delay:(index/4)}}}
+        >
+            <span className="team-stuff">
+                <h3>{team.name}</h3>
+                <img className="team-logo" src={team.logo}></img>
+            </span>
+            <span className="team-rest">
+                <span>
+                    <p>Series Score</p>
+                    <p>{team.wins} - {team.losses}</p>
+                </span>
+            </span>
+                <span className="team-players">
+                    <h3>{team.player1}</h3>
+                    <h3>{team.player2}</h3>
+                    <h3>{team.player3}</h3>
+                </span>
+        </motion.div>
     )
 }
