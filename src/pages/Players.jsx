2839230141d22161d7924e5d455b1b7f1f6ac758
BronzeKennen <sotoskand3@gmail.com>
2023-08-26 19:22:07 +0300

    Changed players to leaderboards, and made it a table with all stats

--- src/pages/Players.jsx
+++ src/pages/Players.jsx
@@ -1,5 +1,8 @@
 import "../styles/Players.css"
 import { createClient } from '@supabase/supabase-js'
+import { faArrowUp, faArrowDown} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
 
 const supabase = createClient('https://cwxskbsjnovkyqeloumw.supabase.co', import.meta.env.VITE_SUPABASE_API_KEY)
 
@@ -11,31 +14,55 @@ let { data: teams } = await supabase
   .from('teams')
   .select('*')
 
-export default function Tournaments() {
+export default function Leaderboard() {
   return (
     <>
     <div className="tour-tab">
-      <h1 id="tour-title">Players</h1>
+      <h1 id="tour-title">Leaderboard</h1>
     </div>
-    <div className="tournament-container">
-      {players.map(player => {
-        return(<PlayerComponent player={player} key={player}/>)
-      })}
-    </div>
+    <table className="tournament-container">
+    <colgroup>
+      <col style={{ width: "20%" }} />
+      <col style={{ width: "10%" }} />
+      <col style={{ width: "10%" }} />
+      <col style={{ width: "10%" }} />
+      <col style={{ width: "10%" }} />
+      <col style={{ width: "10%" }} />
+      <col style={{ width: "10%" }} />
+      <col style={{ width: "10%" }} />
+    </colgroup>
+    <tbody>
+      <tr>
+        <th><FontAwesomeIcon icon={faArrowUp}/> Player Name</th>
+        <th><FontAwesomeIcon icon={faArrowUp}/> Team</th>
+        <th><FontAwesomeIcon icon={faArrowUp}/> Goals</th>
+        <th><FontAwesomeIcon icon={faArrowUp}/> Assists</th>
+        <th><FontAwesomeIcon icon={faArrowUp}/> Saves</th>
+        <th><FontAwesomeIcon icon={faArrowUp}/> Shots</th>
+        <th><FontAwesomeIcon icon={faArrowUp}/> Shot %</th>
+        <th><FontAwesomeIcon icon={faArrowUp}/> MVP</th>
+      </tr>
+          {players.map(player => {
+            return <PlayerComponent player={player} key={player} />;
+          })}
+    </tbody>
+    </table>
     </>
   )
 }
 
-function PlayerComponent(player) {
+function PlayerComponent(props) {
+  const {player} = props;
   return(
-    <div className="tour-component">
-      <div className="name-team">
-        <h1>{player.player.name}</h1>
-        <h2>{player.player.team}</h2>
-      </div>
-      <span>stat1</span>
-      <span>Team</span>
-      <button>idk</button>
-    </div>
+    <tr>
+      <td className="player-name">{player.name}</td>
+      <td>{player.team}</td>
+      <td>{player.goals}</td>
+      <td>{player.assists}</td>
+      <td>{player.saves}</td>
+      <td>{player.shots}</td>
+      <td>{player.shooting_percentage} </td>
+      <td>{player.mvp}</td>
+    </tr>
   )
 }
