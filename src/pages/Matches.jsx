62864ac9bb097ac79a29a7a155236f42e26796d2
BronzeKennen <sotoskand3@gmail.com>
2023-08-28 18:52:30 +0300

    Setup database and imported replays, created component for each replay

--- src/pages/Matches.jsx
+++ src/pages/Matches.jsx
@@ -1,42 +1,59 @@
 import "../styles/Matches.css"
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient('https://cwxskbsjnovkyqeloumw.supabase.co', import.meta.env.VITE_SUPABASE_API_KEY)
+
+
+let { data: replay_file_data, error } = await supabase
+  .from('replay_file_data')
+  .select('*')
+
+let replays = []
+for(let i = 0; i < replay_file_data.length; i += 2) {
+    replays.push([replay_file_data[i],replay_file_data[i+1]]);
+}
 
 export default function Matches() {
     return(
-        <Match/>
+        replays.map((replay,index) => {
+            return(
+                <Match winner={replay[0]} loser={replay[1]} key={index}/>
+            )
+        })
     )
 }
 
 function Match(props) {
-    const {match} = props;
-
+    const {winner, loser} = props;
+    console.log(winner);
     return(
         <div className="match-bg">
             <table className="match-info">
                 <span className="general-stats">
                     <h2>Regular Season - Week (insert number)</h2>
-                    <h2>Map - Date</h2>
+                    <h2>{winner.map} - {winner.date}</h2>
                 </span>
-                <h3>Result</h3>
+                <p>{winner.goals} - {loser.goals}</p>
                 <tr className="teams-presentation">
                     <td className="team1">
-                        <h2>Team 1</h2>
+                        <h2>{winner['team name']}</h2>
                         <span>Team Logo</span>
-                        <span>score</span>
-                        <span>goals</span>
-                        <span>assists</span>
-                        <span>saves</span>
-                        <span>shots</span>
-                        <span>Shot %</span>
+                        <span>Score: {winner.score}</span>
+                        <span>Goals: {winner.goals}</span>
+                        <span>Assists: {winner.assists}</span>
+                        <span>Saves: {winner.saves}</span>
+                        <span>Shots:{winner.shots}</span>
+                        <span>Shot %: {winner.shooting_percentage}</span>
                     </td>
                     <td className="team2">
-                        <h2>Team 2</h2>
+                        <h2>{loser['team name']}</h2>
                         <span>Team Logo</span>
-                        <span>score</span>
-                        <span>goals</span>
-                        <span>assists</span>
-                        <span>saves</span>
-                        <span>shots</span>
-                        <span>Shot %</span>
+                        <span>Score: {loser.score}</span>
+                        <span>Goals: {loser.goals}</span>
+                        <span>Assists: {loser.assists}</span>
+                        <span>Saves: {loser.saves}</span>
+                        <span>Shots:{loser.shots}</span>
+                        <span>Shot %: {loser.shooting_percentage}</span>
                     </td>
                 </tr>
             </table>
